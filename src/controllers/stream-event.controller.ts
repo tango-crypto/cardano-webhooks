18ddda429a374fb1116f346549049b0ba408bd4d
Javier Toledo <javiertc86@gmail.com>
2024-07-10 16:47:25 -0400

    fix-app-name

--- src/controllers/stream-event.controller.ts
+++ src/controllers/stream-event.controller.ts
@@ -13,29 +13,29 @@ export class StreamEventController {
     private readonly streamEventService: StreamEventService
   ) {}
 
-  @EventPattern('new_epoch')
-  async onNewEpoch(@Payload() epoch: Epoch, @Ctx() context: KafkaContext) {
-    console.log(`Process new stream event (epoch) ${'-'.repeat(50)}`);
-    console.log('EPOCH', JSON.stringify(epoch));
-    await this.streamEventService.onNewEpoch(epoch);
-    await Utils.commitOffsets(context);
-  }
+  // @EventPattern('new_epoch')
+  // async onNewEpoch(@Payload() epoch: Epoch, @Ctx() context: KafkaContext) {
+  //   console.log(`Process new stream event (epoch) ${'-'.repeat(50)}`);
+  //   console.log('EPOCH', JSON.stringify(epoch));
+  //   await this.streamEventService.onNewEpoch(epoch);
+  //   await Utils.commitOffsets(context);
+  // }
 
-  @EventPattern('new_block')
-  async onNewBlock(@Payload() block: Block, @Ctx() context: KafkaContext) {
-    console.log(`Process new stream event (block) ${'-'.repeat(50)}`);
-    console.log('BLOCK', JSON.stringify(block));
-    await this.streamEventService.onNewBlock(block);
-    await Utils.commitOffsets(context);
-  }
+  // @EventPattern('new_block')
+  // async onNewBlock(@Payload() block: Block, @Ctx() context: KafkaContext) {
+  //   console.log(`Process new stream event (block) ${'-'.repeat(50)}`);
+  //   console.log('BLOCK', JSON.stringify(block));
+  //   await this.streamEventService.onNewBlock(block);
+  //   await Utils.commitOffsets(context);
+  // }
 
-  @EventPattern('new_delegation')
-  async onNewDelegation(@Payload() delegation: Delegation, @Ctx() context: KafkaContext) {
-    console.log(`Process new stream event (delegation) ${'-'.repeat(50)}`);
-    console.log('DELEGATION', JSON.stringify(delegation));
-    await this.streamEventService.onNewDelegation(delegation);
-    await Utils.commitOffsets(context);
-  }
+  // @EventPattern('new_delegation')
+  // async onNewDelegation(@Payload() delegation: Delegation, @Ctx() context: KafkaContext) {
+  //   console.log(`Process new stream event (delegation) ${'-'.repeat(50)}`);
+  //   console.log('DELEGATION', JSON.stringify(delegation));
+  //   await this.streamEventService.onNewDelegation(delegation);
+  //   await Utils.commitOffsets(context);
+  // }
 
   @EventPattern('new_payment')
   async onNewPayment(@Payload() payment: Payment, @Ctx() context: KafkaContext) {
@@ -44,13 +44,13 @@ export class StreamEventController {
     await this.streamEventService.onNewPayment(payment);
     await Utils.commitOffsets(context);
   }
-  
-  @EventPattern('new_transaction')
-  async onNewTransaction(@Payload() tx: Transaction, @Ctx() context: KafkaContext) {
-    console.log(`Process new stream event (transaction) ${'-'.repeat(50)}`);
-    console.log('TRANSACTION', JSON.stringify(tx));
-    await this.streamEventService.onNewTransaction(tx);
-    await Utils.commitOffsets(context);
-  }
+
+  // @EventPattern('new_transaction')
+  // async onNewTransaction(@Payload() tx: Transaction, @Ctx() context: KafkaContext) {
+  //   console.log(`Process new stream event (transaction) ${'-'.repeat(50)}`);
+  //   console.log('TRANSACTION', JSON.stringify(tx));
+  //   await this.streamEventService.onNewTransaction(tx);
+  //   await Utils.commitOffsets(context);
+  // }
 
 }
